96c0b0069cc2fcbb10e0bbd65f1cf10ac4f3e5e2
PIPI97 <phat01658844@gmail.com>
2022-11-09 00:36:27 +0700

    .

---
+++ maxnumber/src/maxnumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class maxnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size;
+        int[] numarray;
+        do {
+            System.out.println("Size : ");
+            size = sc.nextInt();
+            if (size > 20) {
+                System.out.println(size + " is too large; enter Size < 20");
+            }
+        } while (size > 20);
+        numarray = new int[size];
+        for (int i = 0; i < numarray.length; i++) {
+            System.out.println("Enter num " + (i + 1) + "st");
+            numarray[i] = sc.nextInt();
+        }
+        for (int n : numarray) {
+            System.out.print(n + "\t");
+        }
+        int max = numarray[0];
+        for (int i = 1; i < numarray.length; i++) {
+            if (numarray[i] > max) {
+                max = numarray[i];
+            }
+        }
+        System.out.println("max number is " + max);
+    }
+}
