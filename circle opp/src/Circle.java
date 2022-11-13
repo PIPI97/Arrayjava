4eef063b8e5432e03e1e1d563ce2967e7cdcb3af
PIPI97 <phat01658844@gmail.com>
2022-11-13 16:20:54 +0700

    .

---
+++ circle opp/src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private double radius =1.0;
+    private String color ="red";
+    Circle(){;
+    }
+    Circle(double radius){
+       this.radius= radius;
+    }
+    double getRadius(){
+        return radius;
+    }
+    double getArea(){
+        return Math.PI*this.radius*this.radius;
+    }
+
+}
---
+++ circle opp/src/Testcircle.java
@@ -0,0 +1,6 @@
+public class Testcircle {
+    public static void main(String[] args) {
+        Circle Circle = new Circle();
+        System.out.println(Circle.getRadius()+"\n"+Circle.getArea());
+    }
+}
