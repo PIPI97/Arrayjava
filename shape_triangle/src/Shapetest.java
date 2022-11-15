2c8d2511d45c472df710c99f851dd5649bd58639
PIPI97 <phat01658844@gmail.com>
2022-11-16 00:23:33 +0700

    .

---
+++ shape_triangle/src/Shapetest.java
@@ -0,0 +1,8 @@
+public class Shapetest {
+    public static void main(String[] args) {
+        Shape shape1 = new Shape();
+        System.out.println(shape1);
+        Shape shape2 = new Shape("blue");
+        System.out.println(shape2);
+    }
+}
---
+++ shape_triangle/src/TriagleTest.java
@@ -0,0 +1,9 @@
+public class TriagleTest {
+    public static void main(String[] args) {
+        Triagle triagle1= new Triagle();
+        System.out.println(triagle1);
+        Triagle triagle2= new Triagle(4,3,2,"gray");
+        System.out.println(triagle2);
+    }
+
+}
