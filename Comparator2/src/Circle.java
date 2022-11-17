4895ea61cf5e121b50ea589ee41b82aa72b93a94
PIPI97 <phat01658844@gmail.com>
2022-11-17 17:09:46 +0700

    .

---
+++ Comparator2/src/Circle.java
@@ -0,0 +1,55 @@
+public class Circle {
+
+    private double radius = 1;
+    private String color = "yellow";
+    private Boolean filled = false;
+
+    Circle() {
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    ;
+
+    Circle(double radius, String color, Boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
+
