fea6feb187ed43a790278b4cc710c0305d9071eb
PIPI97 <phat01658844@gmail.com>
2022-11-22 15:41:31 +0700

    .

--- simplelist/src/MyList.java
+++ simplelist/src/MyList.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class MyList<E> {
     private int size = 0;
@@ -12,20 +15,31 @@ public class MyList<E> {
     public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
     }
-    private void ensureCapa(){
+
+    private void ensureCapa() {
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
+
     public void add(E e) {
         if (size == elements.length) {
             ensureCapa();
         }
         elements[size++] = e;
     }
-    public E get(int i) {
+
+        public E get(int i) {
         if (i>= size || i <0) {
             throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
         }
         return (E) elements[i];
     }
+//    public static void main(String[] args) {
+//        ArrayList<Integer> test = new ArrayList<>();
+//        test.add(1);
+//        test.add(2);
+//        test.add(3);
+//        test.add(4);
+//        System.out.println(Arrays.toString(test.toArray()));
+//    }
 }
