57368779c54a4dec3bdc8619b8468a8233e13aad
PIPI97 <phat01658844@gmail.com>
2022-11-24 14:04:13 +0700

    .

---
+++ Stack/src/GenericStackClient.java
@@ -0,0 +1,8 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of Strings");
+        MyGenericStack.stackOfIStrings();
+        System.out.println("\n2. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+    }
+}
